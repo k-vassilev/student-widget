2ac6b4c661222dabde40cdf1d67973bb9ba7919b
Kristian Vassilev <kvasilev@devrix.com>
2021-07-09 16:58:00 +0300

    Register sidebar

--- student-widget.php
+++ student-widget.php
@@ -153,4 +153,48 @@ class Ob_Student_Widget extends WP_Widget {
 add_action( 'widgets_init', function(){
     register_widget( 'Ob_Student_Widget' );
 } );
+
+
+// Register Sidebar
+
+function ob_sidebar(){
+
+	register_sidebar( 
+		array(
+			'name' => 'Student Sidebar',
+			'id' => 'ob-sidebar',
+			'before_title' => '<h4 class="widget-title">',
+			'after_title' => '</h4>'
+		)
+	);
+}
+add_action('widgets_init', 'ob_sidebar');
+
+
+/**
+ * Grab latest post title by an author!
+ *
+ * @param array $data Options for the function.
+ * @return string|null Post title for the latest,* or null if none.
+ */
+
+function my_awesome_func( $data ) {
+  $posts = get_posts( array(
+    'author' => $data['id'],
+  ) );
+ 
+  if ( empty( $posts ) ) {
+    return null;
+  }
+ 
+  return $posts[0]->post_title;
+}
+
+add_action( 'rest_api_init', function () {
+    register_rest_route( 'myplugin/v1', '/author/(?P<id>\d+)', array(
+      'methods' => 'GET',
+      'callback' => 'my_awesome_func',
+    ) );
+  } );
+
  ?>
