dceae505b6da87e20bd296960b49969d6cfa71fe
Kristian Vassilev <kvasilev@devrix.com>
2021-07-12 16:30:54 +0300

    Added comments and formatting

--- student-widget.php
+++ student-widget.php
@@ -4,171 +4,193 @@
  * Description: Adds student widget and shortcode
  * Author: Kristian Vassilev
  * Version: 1.0.0
+ *
+ * @package student-widget
  */
 
-// STUDENT SHORTCODE
+// STUDENT SHORTCODE.
 
-// shortcode logic 
-function ob_student_shortcode($atts) { 
+/**
+ * Shortcode logic
+ *
+ * @param string $atts are all the attributes used in the function.
+ * @return void
+ */
+function ob_student_shortcode( $atts ) {
 
-    // Adds student_id as attribute
-    $attribute = shortcode_atts(array(
-        'student_id' => 'placeholder'
-    ), $atts);
+	// Adds student_id as attribute.
+	$attribute = shortcode_atts( array( 'student_id' => 'placeholder' ), $atts );
 
-    // Checks if the attribute is numeric
-    $student_id_checker = intval($attribute['student_id']);
-    if(!$student_id_checker){
-        return 'Student ID must be numeric';
-    }
+	// Checks if the attribute is numeric.
+	$student_id_checker = intval( $attribute['student_id'] );
+	if ( ! $student_id_checker ) {
+		return 'Student ID must be numeric';
+	}
 
-    // Stores the info needed for the query (p = post ID)
-    $post_info = array(
-        'post_type' => 'student',
-        'p'=> $student_id_checker
-    );
+	// Stores the info needed for the query (p = post ID).
+	$post_info = array(
+		'post_type' => 'student',
+		'p'         => $student_id_checker,
+	);
 
-    // Query to get the student info based on the post_info
-    $student_query = new WP_Query($post_info);
+	// Query to get the student info based on the post_info.
+	$student_query = new WP_Query( $post_info );
 
-    // Returns the content of the student if it exists
-    if(!$student_query -> have_posts()) {
-        return 'No student found';
-    }else{ 
-        $student_query -> the_post();?>
-        <div style="width:300px;">
-        <h4 style="text-align: center; padding:5px;"><?php the_title();?></h4><?php
-        the_post_thumbnail('thumbnail');?>
-        <h4 style="text-align: center; padding:5px;">Student is: <?php echo get_post_meta(get_the_ID(),'_student_class_value_key')[0];?></h4>
-        </div><?php
-    }
-} 
+	// Returns the content of the student if it exists.
+	if ( ! $student_query -> have_posts() ) {
+		return 'No student found';
+	} else {
+		$student_query -> the_post();?>
+		<div style="width:300px;">
+		<h4 style="text-align: center; padding:5px;"><?php the_title(); ?></h4>
+		<?php
+		the_post_thumbnail( 'thumbnail' );
+		?>
+		<h4 style="text-align: center; padding:5px;">Student is: <?php echo get_post_meta( get_the_ID(), '_student_class_value_key' )[0]; ?></h4>
+		</div>
+		<?php
+	}
+}
 
-// shortcode registration
-add_shortcode('student', 'ob_student_shortcode'); 
+// shortcode registration.
+add_shortcode( 'student', 'ob_student_shortcode' );
 
 
-// STUDENT WIDGET
+// STUDENT WIDGET.
 
-// Creating the widget 
+// Creating the widget.
+/**
+ * Student widget class.
+ */
 class Ob_Student_Widget extends WP_Widget {
-  
-    public function __construct() {
-        $widget_ops = array(
-            'classname' => 'ob-student-widget',
-            'description' => 'Student Profiles Widget',
-        );
-        parent::__construct( 'ob_student', 'Student Profiles', $widget_ops );
-    }
+	public function __construct() {
+		$widget_ops = array(
+			'classname'   => 'ob-student-widget',
+			'description' => 'Student Profiles Widget',
+		);
+		parent::__construct( 'ob_student', 'Student Profiles', $widget_ops );
+	}
 
-    // Back-End
+	// Back-End.
 
-    public function form( $instance ){
+	public function form( $instance ) {
 
-        // PART 1: Extract the data from the instance variable
-     $instance = wp_parse_args( (array) $instance, array( 'posts_per_page' => '' ) );
-     $posts_per_page = $instance['posts_per_page'];
-     $student_status = $instance['student_status'];   
+		// PART 1: Extract the data from the instance variable.
 
-     // PART 2-3: Display the fields
-     ?>
-     <!-- PART 2: Widget Posts per page field START -->
-     <p>
-      <label for="<?php echo $this->get_field_id('posts_per_page'); ?>">Posts per page: 
-        <input class="widefat" id="<?php echo $this->get_field_id('posts_per_page'); ?>" 
-               name="<?php echo $this->get_field_name('posts_per_page'); ?>" type="text" 
-               value="<?php echo attribute_escape($posts_per_page); ?>" />
-      </label>
-      </p>
-      <!-- Widget Posts per page field END -->
+		$instance       = wp_parse_args( (array) $instance, array( 'posts_per_page' => '' ) );
+		$posts_per_page = $instance['posts_per_page'];
+		$student_status = $instance['student_status'];
 
-     <!-- PART 3: Widget Student Status field START -->
-     <p>
-      <label for="<?php echo $this->get_field_id('text'); ?>">Active / inactive: 
-        <select class='widefat' id="<?php echo $this->get_field_id('student_status'); ?>"
-                name="<?php echo $this->get_field_name('student_status'); ?>" type="text">
-          <option value='active'<?php echo ($student_status=='active')?'selected':''; ?>>
-            Active
-          </option>
-          <option value='inactive'<?php echo ($student_status=='inactive')?'selected':''; ?>>
-            Inactive
-          </option> 
-        </select>                
-      </label>
-     </p>
-     <!-- Widget Student Status field END -->
-     <?php 
+		// PART 2-3: Display the fields.
+		?>
+		<!-- PART 2: Widget Posts per page field START -->
+		<p>
+		<label for="<?php echo $this->get_field_id( 'posts_per_page' ); ?>">Posts per page: 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'posts_per_page' ); ?>" 
+			name="<?php echo $this->get_field_name( 'posts_per_page' ); ?>" type="text" 
+			value="<?php echo esc_attr( $posts_per_page ); ?>" />
+		</label>
+		</p>
+		<!-- Widget Posts per page field END -->
 
-    }
+	<!-- PART 3: Widget Student Status field START -->
+		<p>
+		<label for="<?php echo $this->get_field_id( 'text' ); ?>">Active / inactive: 
+		<select class='widefat' id="<?php echo $this->get_field_id( 'student_status' ); ?>"
+			name="<?php echo $this->get_field_name( 'student_status' ); ?>" type="text">
+			<option value='active'<?php echo ( 'active' === $student_status ) ? 'selected' : ''; ?>>
+			Active
+			</option>
+			<option value='inactive'<?php echo ( 'inactive' === $student_status ) ? 'selected' : ''; ?>>
+			Inactive
+			</option> 
+		</select>                
+		</label>
+		</p>
+		<!-- Widget Student Status field END -->
+		<?php
+	}
 
-    // Front-End
+	// Front-End.
 
-    public function widget( $args, $instance ){
+	public function widget( $args, $instance ) {
 
-        
-        // PART 1: Extracting the arguments + getting the values
-        extract($args, EXTR_SKIP);
-        $posts_per_page = empty($instance['posts_per_page']) ? ' ' : apply_filters('widget_title', $instance['posts_per_page']);
-        $student_status = empty($instance['student_status']) ? '' : $instance['student_status'];
-        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+		// PART 1: Extracting the arguments + getting the values.
+		extract( $args, EXTR_SKIP );
+		$posts_per_page = empty( $instance['posts_per_page'] ) ? ' ' : apply_filters( 'widget_title', $instance['posts_per_page'] );
+		$student_status = empty( $instance['student_status'] ) ? '' : $instance['student_status'];
+		$paged          = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
 
-        if($student_status == 'active'){
-            $loop = new WP_Query( array( 
-                'post_type' => 'student', 
-                'posts_per_page' => $posts_per_page,
-                'paged' => $paged,
-                'meta_key' => '_is_active_student',
-                'meta_value'=> 'true'
-                ) ); 
-        }else if($student_status == 'inactive'){
-            $loop = new WP_Query( array( 
-                'post_type' => 'student', 
-                'posts_per_page' => $posts_per_page,
-                'paged' => $paged,
-                'meta_key' => '_is_active_student',
-                'meta_value'=> 'false'
-                ) ); 
-        }
-        
+		if ( 'active' === $student_status ) {
+			$loop = new WP_Query(
+				array(
+					'post_type'      => 'student',
+					'posts_per_page' => $posts_per_page,
+					'paged'          => $paged,
+					'meta_key'       => '_is_active_student',
+					'meta_value'     => 'true',
+				)
+			);
+		} elseif ( 'inactive' === $student_status ) {
+			$loop = new WP_Query( 
+				array(
+					'post_type'      => 'student',
+					'posts_per_page' => $posts_per_page,
+					'paged'          => $paged,
+					'meta_key'       => '_is_active_student',
+					'meta_value'     => 'false',
+				)
+			);
+		}
 
-        echo (isset($before_widget)?$before_widget:'');
+		echo ( isset( $before_widget ) ? $before_widget : '' );
 
-        if ( have_posts($loop) ){
-            while ( $loop -> have_posts() ) : $loop -> the_post();?>
-            
-            <div style="width: 300px;"><?php
-                the_post_thumbnail( 'standard' );    
-                the_title();?>
-                <?php echo get_post_meta(get_the_ID(),'_student_class_value_key')[0];?>
-            </div><?php
+		if ( have_posts( $loop ) ) {
+			while ( $loop -> have_posts() ) : $loop -> the_post(); 
+				?>
+				<div style="width: 300px;">
+				<?php
+				the_post_thumbnail( 'standard' );
+				the_title();
+				?>
+				<?php echo get_post_meta( get_the_ID(), '_student_class_value_key' )[0]; ?>
+				</div><?php
 
-            endwhile;
-        }
-
-        echo (isset($after_widget)?$after_widget:'');
-    }
+			endwhile;
+		}
+		echo ( isset( $after_widget ) ? $after_widget : '' );
+	}
 }
 
 
-add_action( 'widgets_init', function(){
-    register_widget( 'Ob_Student_Widget' );
-} );
+
+add_action(
+	'widgets_init',
+	function() {
+		register_widget( 'Ob_Student_Widget' );
+	}
+);
 
 
-// Register Sidebar
+// Register Sidebar.
 
-function ob_sidebar(){
+/**
+ * Creates the sidebar
+ *
+ * @return void
+ */
+function ob_sidebar() {
 
-	register_sidebar( 
+	register_sidebar(
 		array(
-			'name' => 'Student Sidebar',
-			'id' => 'ob-sidebar',
+			'name'         => 'Student Sidebar',
+			'id'           => 'ob-sidebar',
 			'before_title' => '<h4 class="widget-title">',
-			'after_title' => '</h4>'
+			'after_title'  => '</h4>',
 		)
 	);
 }
-add_action('widgets_init', 'ob_sidebar');
+add_action( 'widgets_init', 'ob_sidebar' );
 
 
 /**
@@ -177,24 +199,29 @@ add_action('widgets_init', 'ob_sidebar');
  * @param array $data Options for the function.
  * @return string|null Post title for the latest,* or null if none.
  */
-
 function my_awesome_func( $data ) {
-  $posts = get_posts( array(
-    'author' => $data['id'],
-  ) );
- 
-  if ( empty( $posts ) ) {
-    return null;
-  }
- 
-  return $posts[0]->post_title;
+	$posts = get_posts(
+		array(
+			'author' => $data['id'],
+		)
+	);
+
+	if ( empty( $posts ) ) {
+		return null;
+	}
+	return $posts[0]->post_title;
 }
 
-add_action( 'rest_api_init', function () {
-    register_rest_route( 'myplugin/v1', '/author/(?P<id>\d+)', array(
-      'methods' => 'GET',
-      'callback' => 'my_awesome_func',
-    ) );
-  } );
-
- ?>
+add_action(
+	'rest_api_init',
+	function () {
+		register_rest_route(
+			'myplugin/v1',
+			'/author/(?P<id>\d+)',
+			array(
+				'methods'  => 'GET',
+				'callback' => 'my_awesome_func',
+			)
+		);
+	}
+);
