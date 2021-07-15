bba92066a0644ccffd574b323d308cd8242a4b40
Kristian Vassilev <kvasilev@devrix.com>
2021-07-15 18:06:24 +0300

    Formatting

--- student-widget.php
+++ student-widget.php
@@ -8,56 +8,6 @@
  * @package student-widget
  */
 
-// STUDENT SHORTCODE.
-
-/**
- * Shortcode logic
- *
- * @param string $atts are all the attributes used in the function.
- * @return void
- */
-function ob_student_shortcode( $atts ) {
-
-	// Adds student_id as attribute.
-	$attribute = shortcode_atts( array( 'student_id' => 'placeholder' ), $atts );
-
-	// Checks if the attribute is numeric.
-	$student_id_checker = intval( $attribute['student_id'] );
-	if ( ! $student_id_checker ) {
-		return 'Student ID must be numeric';
-	}
-
-	// Stores the info needed for the query (p = post ID).
-	$post_info = array(
-		'post_type' => 'student',
-		'p'         => $student_id_checker,
-	);
-
-	// Query to get the student info based on the post_info.
-	$student_query = new WP_Query( $post_info );
-
-	// Returns the content of the student if it exists.
-	if ( ! $student_query -> have_posts() ) {
-		return 'No student found';
-	} else {
-		$student_query -> the_post();?>
-		<div style="width:300px;">
-		<h4 style="text-align: center; padding:5px;"><?php the_title(); ?></h4>
-		<?php
-		the_post_thumbnail( 'thumbnail' );
-		?>
-		<h4 style="text-align: center; padding:5px;">Student is: <?php echo get_post_meta( get_the_ID(), '_student_class_value_key' )[0]; ?></h4>
-		</div>
-		<?php
-	}
-}
-
-// shortcode registration.
-add_shortcode( 'student', 'ob_student_shortcode' );
-
-
-// STUDENT WIDGET.
-
 // Creating the widget.
 /**
  * Student widget class.
